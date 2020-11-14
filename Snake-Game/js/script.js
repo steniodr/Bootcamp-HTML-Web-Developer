fa8902c0007cfb4f0fe9b6ed3ace4edf597474f3
Stenio D. Rapchan <stenio.dias@fastcash.com.br>
2020-11-14 19:49:29 -0300

    Add keywords WSAD in game and Update intro

--- Snake-Game/js/script.js
+++ Snake-Game/js/script.js
@@ -33,10 +33,10 @@ function drawFood(color) {
 document.addEventListener('keydown', update);
 
 function update (event) {
-    if (event.keyCode == 37 && direction != 'right')    direction = 'left'; 
-    if (event.keyCode == 38 && direction != 'down')     direction = 'up'; 
-    if (event.keyCode == 39 && direction != 'left')     direction = 'right'; 
-    if (event.keyCode == 40 && direction != 'up')       direction = 'down'; 
+    if ((event.keyCode == 37 || event.keyCode == 65) && direction != 'right')    direction = 'left'; 
+    if ((event.keyCode == 38 || event.keyCode == 87) && direction != 'down')     direction = 'up'; 
+    if ((event.keyCode == 39 || event.keyCode == 68) && direction != 'left')     direction = 'right'; 
+    if ((event.keyCode == 40 || event.keyCode == 83) && direction != 'up')       direction = 'down'; 
 }
 
 function startGame() {
@@ -58,7 +58,7 @@ function startGame() {
         createBG('lightgreen');
         createSnake('green');
         drawFood('red');
-    } else if (points >= 10 && points < 15){
+    } else if (points >= 10 && points < 20){
         let mode = document.getElementById('game-mode')
         mode.innerHTML = "Deserto";
         mode.style.color = "#DE681F";
@@ -67,7 +67,7 @@ function startGame() {
         createBG('#DE681F');
         createSnake('#EB9F15');
         drawFood('#EB2915');
-    } else if (points >= 15 && points < 20){
+    } else if (points >= 20 && points < 30){
         let mode = document.getElementById('game-mode')
         mode.innerHTML = "Por do Sol";
         mode.style.color = "#FF761A";
@@ -76,7 +76,7 @@ function startGame() {
         createBG('#FF761A');
         createSnake('#D1201C');
         drawFood('#FFFFFF');
-    } else if (points >= 20){
+    } else if (points >= 30){
         let mode = document.getElementById('game-mode')
         mode.innerHTML = "Espaço";
         mode.style.color = "#575757";
